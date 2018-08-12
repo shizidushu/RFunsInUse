aa1a669aca339b3f49b81574ba40ad7b1a8e6a9f
shizidushu <shizidushu@gmail.com>
2018-08-12 05:11:22 +0000

    get shiny input values

--- R/shiny.R
+++ R/shiny.R
@@ -27,3 +27,24 @@ downloadfile <- function(input,
       content = function(path) {writexl::write_xlsx(x, path)},
       contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 }
+
+
+#' Concatenate shiny input value into character string
+#' 
+#' @param input shiny input object. Always should be input
+#' @param exclude_start_chr a character vector. If the name of input start with the character, exclude its value(s)
+#' @export
+get_shiny_input_values <- function(input, exclude_start_chr = "table") {
+  input_names <- names(input)
+  
+  if(!rlang::is_character(exclude_start_chr)) stop("exclude_start_chr is not character")
+  
+  for (i in seq_along(exclude_start_chr)) {
+    input_names <- input_names[!startsWith(input_names, exclude_start_chr[i])] 
+  }
+  
+  input_names %>%
+    purrr::map(~input[[.x]] %>%
+                 paste(sep = "_", collapse = "")) %>%
+    paste0(sep = "_", collapse = "")
+}
